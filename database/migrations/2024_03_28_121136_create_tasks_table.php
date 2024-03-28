1de577b57c82a6b3bb03988e9fe8717eee7078ae
Brightlyviryaa <virya.brightly@gmail.com>
2024-03-28 19:23:47 +0700

    Menyelesaikan migration task

--- database/migrations/2024_03_28_121136_create_tasks_table.php
+++ database/migrations/2024_03_28_121136_create_tasks_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->text("description");
+            $table->date("due_date");
+            $table->foreignId("user_id")->constrained();
             $table->timestamps();
         });
     }
