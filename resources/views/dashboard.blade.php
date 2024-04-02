bb093978a29d1f8741cc1d1fbb7744df6aeeb5fd
Malvin Wijaya <malvinw123@gmail.com>
2024-04-02 09:03:11 +0700

    memperbaiki date

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,7 +23,7 @@
                 <h1 class="text-3xl font-bold">To-do List</h1>
                 <h2 class="text-md">By Gerda SOWULO</h2>    
             </div>    
-            <p class="mr-4">Now: Thu, 28 Maret 2024</p>
+            <p id="dateContainer" class="mr-4">Now: Thu, 28 Maret 2024</p>
             <button class="bg-blue-400 text-white px-4 py-2 rounded-md">Log Out</button>  
         </nav>
 
@@ -77,5 +77,81 @@
                 </div>
             </div>
         </div>
-    </div>    
+    </div>
+    
+    <script>
+        // Now: Thu, 28 Maret 2024
+        let date = new Date();
+        
+        const dateContainer = document.querySelector("#dateContainer");
+
+        let day
+        switch(date.getDay()){
+            case 0: 
+                day = "Minggu"
+                break
+            case 1:
+                day = "Senin"
+                break
+            case 2:
+                day = "Selasa"
+                break
+            case 3:
+                day = "Rabu"
+                break
+            case 4:
+                day = "Kamis"
+                break
+            case 5:
+                day = "Jumat"
+                break
+            case 6:
+                day = "Sabtu"
+                break                    
+        }
+
+        let month
+        switch(date.getMonth()){
+            case 0:
+                month = "Januari"
+                break
+            case 1:
+                month = "Februari"
+                break
+            case 2:
+                month = "Maret"
+                break
+            case 3:
+                month = "April"
+                break  
+            case 4:
+                month = "Mei"
+                break
+            case 5:
+                month = "Juni"
+                break
+            case 6:
+                month = "Juli"
+                break
+            case 7:
+                month = "Agustus"
+                break
+            case 8:
+                month = "September"
+                break
+            case 9:
+                month = "Oktober"
+                break
+            case 10:
+                month = "November"
+                break
+            case 11:
+                month = "Desember"         
+                break                                 
+        }
+
+        let dateString = "Now: " + day + ", " + date.getDate() + " " + month + " " + date.getFullYear();
+        dateContainer.innerHTML = dateString;
+
+    </script>
 @endsection
